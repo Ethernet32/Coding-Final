9b8efbe2d864006be3bc2aab91a07bf6ed8abb36
ethernet32 <ethan.ralphs@ucas-edu.net>
2024-12-05 11:19:22 -0700

    put in player sprite and rotation

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import pygame
 from math import atan2, degrees
 # tank = pygame.image.load('Sprite0.png')
-wn = pygame.display.set_mode((400, 400))
+wn = pygame.display.set_mode((650, 750))
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('img/bird.png')
+        self.image = pygame.image.load('img\player.png')
         self.x = 0
         self.y = 0
         self.rect = self.image.get_rect()
+        self.health = 100
+    def resize_image(self):
+        self.image = pygame.transform.scale(self.image, (60,60))
 
     def point_at(self, x, y):
         rotated_image = pygame.transform.rotate(self.image, degrees(atan2(x-self.rect.x, y-self.rect.y)))
@@ -18,6 +21,7 @@ class Player(pygame.sprite.Sprite):
         wn.blit(rotated_image, new_rect.topleft)
 
 player = Player()
+player.resize_image()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
