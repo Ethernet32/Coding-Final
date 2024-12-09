3b8b21b695b3ada3ee81fd8b4eb4ea022a594834
ethernet32 <ethan.ralphs@ucas-edu.net>
2024-12-09 11:49:40 -0700

    INFINATE FLOOORS

--- game.py
+++ game.py
@@ -6,8 +6,12 @@ class Game(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.ground = pygame.image.load('img/ground.png')
+        self.groundrect = self.ground.get_rect()
         self.player = pygame.image.load('img/player.png')
         self.playerrect = self.player.get_rect()
+        self.bullet = pygame.image.load('img/bullet.png')
+        self.bullet_list = []
+        self.bulletrect = self.bullet.get_rect()
         self.floor_x = 0
         self.floor_y = 0
         self.health = 100
@@ -15,13 +19,15 @@ class Game(pygame.sprite.Sprite):
         self.rotated_image = self.player
         self.new_rect = self.playerrect
         self.playerrect.center = (850/2, 750/2)
+        self.groundrect.center = ((2550/2, 2250/2))
 
     def resize_images(self):
         self.player = pygame.transform.scale(self.player, (64, 60))
-        self.ground = pygame.transform.scale(self.ground, (850, 750))
+        self.ground = pygame.transform.scale(self.ground, (2550, 2250))
+        self.bullet = pygame.transform.scale(self.bullet, (10, 20))
 
     def show_floor(self, screen):
-        screen.blit(self.ground, (self.floor_x, self.floor_y))
+        screen.blit(self.ground, (self.floor_x-850, self.floor_y-850))
 
     def point_at(self, x, y):
         angle = atan2(y - self.playerrect.centery, x - self.playerrect.centerx)
@@ -31,11 +37,28 @@ class Game(pygame.sprite.Sprite):
     def show_player(self, screen):
         screen.blit(self.rotated_image, self.new_rect.topleft)
 
+    def add_bullet(self,screen):
+        bullet = self.bullet.get_rect(midbottom = (0,70))
+        self.bullet_list.append(bullet)
+
+    def show_bullets(self, screen):
+        for bullet in self.bullet_list:
+            screen.blit(self.bullet,bullet)
+
     def move_right(self):
-        self.floor_x += 1
+        self.floor_x -= 1.5
+        if self.floor_x <= -850:
+            self.floor_x += 850
     def move_left(self):
-        self.floor_x -= 1
+        self.floor_x += 1.5
+        if self.floor_x >= 850:
+            self.floor_x -= 850
     def move_up(self):
-        self.floor_y += 1
+        self.floor_y += 1.5
+        if self.floor_y >= 850:
+            self.floor_y -= 850
     def move_down(self):
-        self.floor_y -= 1
+        
+        self.floor_y -= 1.5
+        if self.floor_y <= -850:
+            self.floor_y += 850
--- main.py
+++ main.py
@@ -7,14 +7,14 @@ screen = pygame.display.set_mode((850, 750))
 game = Game()
 game.resize_images()
 clock = pygame.time.Clock()
-game.resize_images()
-
 while True:
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame. MOUSEBUTTONDOWN:
+            game.add_bullet(screen)
+    
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_d]:
         game.move_right()
@@ -24,8 +24,10 @@ while True:
         game.move_up()
     if pressed[pygame.K_s]:
         game.move_down()
-
+    
+    
     game.show_floor(screen)
+    game.show_bullets
     if pygame.mouse.get_focused():
         game.point_at(*pygame.mouse.get_pos())
     game.show_player(screen)
