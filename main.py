9c7b196d69792df3631fe76810e9ff42c7499836
Ethernet32 <ethan.ralphs@ucas-edu.net>
2024-12-12 23:08:51 -0700

    Update main.py
    fixed shooting and started enemy

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import pygame
 import sys
 from game import *
@@ -8,40 +7,42 @@ screen = pygame.display.set_mode((850, 750))
 game = Game()
 game.resize_images()
 clock = pygame.time.Clock()
-SPAWNENEMY = pygame.USEREVENT +1
+
+SPAWNENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(SPAWNENEMY, 1000)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit() 
-            sys.exit() 
-        if event.type == pygame. MOUSEBUTTONDOWN:
-            game.add_bullet()
-            #game.rotate_bullet(*pygame.mouse.get_pos())
-        if event.type == SPAWNENEMY:
-            game.add_enemy()
+            sys.exit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            game.add_bullet(*pygame.mouse.get_pos())
+
         if pygame.mouse.get_focused():
-                game.point_at(*pygame.mouse.get_pos())
-            
+            game.point_at(*pygame.mouse.get_pos())
+
+        if event.type == SPAWNENEMY:
+            game.add_enemy(850/2, 750/2)
+        
+
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_d]:
-        game.move_right()
+        game.move_right(2)
     if pressed[pygame.K_a]:
-        game.move_left()
+        game.move_left(2)
     if pressed[pygame.K_w]:
-        game.move_up()
+        game.move_up(2)
     if pressed[pygame.K_s]:
-        game.move_down()
-    
-    
+        game.move_down(2)
+
     game.show_floor(screen)
     game.show_bullets(screen)
-    game.find_slope(*pygame.mouse.get_pos())
-    game.show_enemy(screen)
+    game.move_bullet()
+    game.move_enemy()
+    game.show_enemy(850/2, 750/2)
     game.show_player(screen)
-
-    
 
     pygame.display.update()
     clock.tick(120)
